9c088c67da410f56c8f3f00576044cde532cfebd
Frazee Design <frazeedesign@gmail.com>
2021-04-27 13:40:43 -0400

    updated section container

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,1 +1,14 @@
-new Vue({comments:!0,mounted:function(){console.log(Array.prototype),console.log(jSQL)}}).$mount("#app");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************************!*\
+  !*** ./resources/js/welcome.js ***!
+  \*********************************/
+new Vue({
+  comments: true,
+  mounted: function mounted() {
+    console.log(Array.prototype);
+    console.log(jSQL);
+  }
+}).$mount('#app');
+/******/ })()
+;
