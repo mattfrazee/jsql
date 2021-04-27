b5c59a59acd273ae7788ffbf6fb26f813aec3c49
Frazee Design <frazeedesign@gmail.com>
2021-04-27 18:36:15 -0400

    updated stuff

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,10 +11,12 @@ document.getElementById('resultDemo').innerHTML = '// result:\n' + JSON.stringif
     people.SELECT(['name', 'gender', 'age']).WHERE(person => (person.gender === 'male')).ORDER_BY('name', 'ASC')
 , null, 4);
 
-window.addEventListener('scroll',function (e) {
+function checkNavScroll(){
     if(window.pageYOffset >= 200){
         document.querySelector('header').style.top = '0px';
     } else {
         document.querySelector('header').style.top = '-'+document.querySelector('header').scrollHeight+'px';
     }
-})
+}
+window.addEventListener('scroll', checkNavScroll);
+checkNavScroll();
